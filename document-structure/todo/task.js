e05b126ec18ea1105e41805e4cdc35132eff9c86
nick <nick.khachaturian@.gmail.com>
2023-06-17 21:34:36 +0300

    Add local storage

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,27 +1,37 @@
-
 const button = document.forms[0][1];
+const tasksList = document.querySelector('.tasks__list');
 
 button.addEventListener('click', e => {
   const input = document.forms[0][0];
-  const tasksList = document.querySelector('.tasks__list');
 
   if (input.value) {
     tasksList.innerHTML += 
-    `<div class="task">
-      <div class="task__title">
-        ${input.value}
-      </div>
-      <a href="#" class="task__remove">&times;</a>
-    </div>`;
+      `<div class="task">
+        <div class="task__title">
+          ${input.value}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+      </div>`;
+
     input.value = '';
   }
+  
+  remove();
+  localStorage.setItem('tasks', tasksList.innerHTML);
+  e.preventDefault();
+});
 
+window.addEventListener('load', () => {
+  tasksList.innerHTML = localStorage.getItem('tasks');
+  remove();
+});
+
+function remove() {
   const crosses = [...document.querySelectorAll('.task__remove')];
 
   crosses.forEach(cross => cross.addEventListener('click', e => {
     e.target.closest('.task').remove();
+    localStorage.setItem('tasks', tasksList.innerHTML); 
     e.preventDefault();
   }));
-
-  e.preventDefault();
-});
+}
