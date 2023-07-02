6f5022512854362d5e86e33eff102b49f16ad4f1
nick <nick.khachaturian@.gmail.com>
2023-07-02 20:28:07 +0300

    Completed the first task

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,1 +1,42 @@
- 
+const items = document.getElementById('items');
+const loader = document.querySelector('.loader.loader_active');
+const url = 'https://students.netoservices.ru/nestjs-backend/slow-get-courses';
+
+window.addEventListener('load', () => {
+  if (localStorage.hasOwnProperty('exchange-rates')) {
+    loader.classList.remove('loader_active');
+    items.innerHTML = localStorage.getItem('exchange-rates');
+    console.log('loaded exchange rate data from local storage');
+  }
+});
+
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('readystatechange', () => {
+  if (xhr.readyState === xhr.DONE) {
+    const valute = JSON.parse(xhr.responseText).response.Valute;
+
+    loader.classList.remove('loader_active');
+    items.innerHTML = '';
+
+    for (let el in valute) {
+      items.innerHTML +=
+        `<div class="item">
+          <div class="item__code">
+            ${valute[el].CharCode}
+          </div>
+          <div class="item__value">
+            ${valute[el].Value}
+          </div>
+          <div class="item__currency">
+            руб.
+          </div>
+        </div>`
+    }
+    localStorage.setItem('exchange-rates', items.innerHTML);
+    console.log('loaded/updated exchange rate data');
+  }
+});
+
+xhr.open('GET', url);
+xhr.send();
